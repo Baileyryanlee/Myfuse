6f686f209331fea9a27a22ce33a8cb706c31002c
Baileyryanlee <151549840+Baileyryanlee@users.noreply.github.com>
2026-01-17 12:52:09 -0600

    Update FuseHooks_Ranged.h

--- soh/soh/Enhancements/Fuse/Hooks/FuseHooks_Ranged.h
+++ soh/soh/Enhancements/Fuse/Hooks/FuseHooks_Ranged.h
@@ -6,12 +6,12 @@
 extern "C" {
 #endif
 
-void FuseHooks_OnArrowProjectileFired(PlayState* play, int32_t arrowParams);
-void FuseHooks_OnHookshotShotStarted(PlayState* play);
-void FuseHooks_OnHookshotEnemyHit(PlayState* play);
-void FuseHooks_OnHookshotLatched(PlayState* play);
-void FuseHooks_OnHookshotRetracted(PlayState* play);
-void FuseHooks_OnHookshotKilled(PlayState* play);
+	void FuseHooks_OnArrowProjectileFired(struct PlayState* play, int32_t isSeed);
+	void FuseHooks_OnHookshotShotStarted(PlayState* play);
+	void FuseHooks_OnHookshotEnemyHit(PlayState* play);
+	void FuseHooks_OnHookshotLatched(PlayState* play);
+	void FuseHooks_OnHookshotRetracted(PlayState* play);
+	void FuseHooks_OnHookshotKilled(PlayState* play);
 
 #ifdef __cplusplus
 }
